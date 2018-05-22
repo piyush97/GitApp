0539b4722ca29e1eab16a15f51464269ef636760
Piyush Mehta <me@piyushmehta.com>
2018-05-22 15:59:56 +0530

    APi key added

--- gitapp/src/App.js
+++ gitapp/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+import theme from './theme';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="container">
+        <ReactiveBase
+          app="gitapp-Piyush"
+          credentials="oNNmd9afg:22b783fd-7ed2-46fc-85bc-7ddb976e7897"
+          type="gitapp-Piyush"
+          theme={theme}
+        >
+          <nav className="navbar">
+            <div className="title">gitApp</div>
+          </nav>
+        </ReactiveBase>
+      </section>
     );
   }
 }
-
 export default App;
